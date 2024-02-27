63f0332d6823b365b047ef0754266d3b47dfce91
o2real <136673119+o2real@users.noreply.github.com>
2024-02-27 14:40:13 +0000

    reeborg

--- main.py
+++ main.py
@@ -1,9 +1,24 @@
-print("Hello")
-num_char = len("Hello")
-print(num_char)
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
 
-def my_function():
-  print("Hello")
-  print("Bye")
+def jump():
+    turn_left()
+    while wall_on_right():
+        move()
 
-my_function()
+    turn_right()
+    move()
+    turn_right()
+
+    while front_is_clear():
+        move()
+    turn_left()
+
+while not at_goal():
+    if wall_in_front():
+        jump()
+    else:
+        move()
+
