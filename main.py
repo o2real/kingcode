d7139938c3f45f13f1c3ee4f2c290e25a2a49d16
o2real <136673119+o2real@users.noreply.github.com>
2024-03-04 02:43:37 +0000

    hangman3

--- main.py
+++ main.py
@@ -1,14 +1,31 @@
 
-word_list = ["ardvark", "baboon", "camel"]
-
 import random
-
+word_list = ["aardvark", "baboon", "camel"]
 chosen_word = random.choice(word_list)
 
-guess = input("Guess a letter: ").lower()
+print(f'Pssst, the solution is {chosen_word}.')
 
-for letter in chosen_word:
-    if letter == guess :
-        print("right")
-    else :
-        print("wrong")
+display = []
+word_length = len(chosen_word)
+for _ in range(word_length) :
+    display += "_"
+print(display)
+
+end_of_game = False
+
+while not end_of_game :
+
+  guess = input("Guess a letter: ").lower()
+  
+  for position in range(word_length):
+    letter = chosen_word[position]
+    if letter == guess:
+        display[position] = letter
+  
+  print(display)
+
+  if not "_" in display :
+    end_of_game = True
+    print("You Win!")
+  
+  # range , for 복습
