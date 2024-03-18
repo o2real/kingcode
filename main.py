42a4fdbacf47326b58bbd4251ccb96cfd9dd8a6e
o2real <136673119+o2real@users.noreply.github.com>
2024-03-18 00:44:16 +0000

    day8 (2)

--- main.py
+++ main.py
@@ -3,23 +3,32 @@
 # Write 3 print statements inside the function.
 # Call the greet() function and run your code.
 
-def greet() :
-  print("hello world!")
-  print("how do you do?")
-  print("Isn't the weater nice today?")
+# def greet() :
+#   print("hello world!")
+#   print("how do you do?")
+#   print("Isn't the weater nice today?")
 
 
-greet()
+# greet()
 
 
-#
+# #
 
-def greet_with_name(name):
-  print(f"hello {name}!")
-  print(f"how do you do {name}?")
-  print(f"Isn't the weater nice today?")
+# def greet_with_name(name):
+#   print(f"hello {name}!")
+#   print(f"how do you do {name}?")
+#   print(f"Isn't the weater nice today?")
 
-greet_with_name("Jioh")
+# greet_with_name("Jioh")
 
+
+#Functions with more than 1 input
+def greet_with(name, location) :
+  print(f"Hello {name}")
+  print(f"What is it like in {location}")
+
+# greet_with("Jioh", "Korea")
+
+greet_with(location="Korea", name="Jioh")
 
 #range , for 복습
