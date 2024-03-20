8dcdf7dbddb724bd086d183656de1f72d2c4e435
o2real <136673119+o2real@users.noreply.github.com>
2024-03-20 15:02:51 +0000

    day8

--- main.py
+++ main.py
@@ -48,3 +48,21 @@ test_h = int(input()) # Height of wall (m)
 test_w = int(input()) # Width of wall (m)
 coverage = 5
 paint_calc(height=test_h, width=test_w, cover=coverage)
+
+#
+
+
+def prime_checker(number) :
+  is_prime = True
+  for i in range(2, number) :
+    if number % i == 0 :
+      is_prime = False
+
+  if is_prime:
+    print("It's a prime number.")
+  else:
+    print("It's not a prime number.")
+
+number = int(input("choose a number."))
+
+prime_checker(number)
