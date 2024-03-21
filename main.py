d73de964f25be30d742f5489882b09d1e5e358c5
o2real <136673119+o2real@users.noreply.github.com>
2024-03-21 11:27:27 +0000

    Day8 암호

--- main.py
+++ main.py
@@ -1,68 +1,29 @@
-# Review: 
-# Create a function called greet(). 
-# Write 3 print statements inside the function.
-# Call the greet() function and run your code.
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z','a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
-# def greet() :
-#   print("hello world!")
-#   print("how do you do?")
-#   print("Isn't the weater nice today?")
+direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+text = input("Type your message:\n").lower()
+shift = int(input("Type the shift number:\n"))
 
+#TODO-1: Create a function called 'encrypt' that takes the 'text' and 'shift' as inputs.
+def encrypt (plain_text, shift_amount):
+  cipher_text = ""
+  for letter in plain_text:
+    position = alphabet.index(letter)
+    new_position = position + shift_amount
+    new_letter = alphabet[new_position]
+    cipher_text += new_letter
+  print(f"The encoded text is {cipher_text}")
+    #TODO-2: Inside the 'encrypt' function, shift each letter of the 'text' forwards in the alphabet by the shift amount and print the encrypted text.  
+    #e.g. 
+    #plain_text = "hello"
+    #shift = 5
+    #cipher_text = "mjqqt"
+    #print output: "The encoded text is mjqqt"
 
-# greet()
+    ##HINT: How do you get the index of an item in a list:
+    #https://stackoverflow.com/questions/176918/finding-the-index-of-an-item-in-a-list
 
+    ##🐛Bug alert: What happens if you try to encode the word 'civilization'?🐛
 
-# #
-
-# def greet_with_name(name):
-#   print(f"hello {name}!")
-#   print(f"how do you do {name}?")
-#   print(f"Isn't the weater nice today?")
-
-# greet_with_name("Jioh")
-
-
-#Functions with more than 1 input
-def greet_with(name, location) :
-  print(f"Hello {name}")
-  print(f"What is it like in {location}")
-
-# greet_with("Jioh", "Korea")
-
-greet_with(location="Korea", name="Jioh")
-
-#range , for 복습
-
-import math 
-
-def paint_calc(height, width, cover) :
-  num_cans = (height * width) / cover
-  round_up_cans = math.ceil(num_cans)
-  print (f"You'll need {round_up_cans} cans of paint.")
-
-# Write your code above this line 👆
-# Define a function called paint_calc() so the code below works.   
-
-# 🚨 Don't change the code below 👇
-test_h = int(input()) # Height of wall (m)
-test_w = int(input()) # Width of wall (m)
-coverage = 5
-paint_calc(height=test_h, width=test_w, cover=coverage)
-
-#
-
-
-def prime_checker(number) :
-  is_prime = True
-  for i in range(2, number) :
-    if number % i == 0 :
-      is_prime = False
-
-  if is_prime:
-    print("It's a prime number.")
-  else:
-    print("It's not a prime number.")
-
-number = int(input("choose a number."))
-
-prime_checker(number)
+#TODO-3: Call the encrypt function and pass in the user inputs. You should be able to test the code and encrypt a message. 
+encrypt (plain_text=text , shift_amount=shift)
