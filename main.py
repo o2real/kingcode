df2d6dddf54348c402ee71a7d7bd259b8bcae2a3
o2real <136673119+o2real@users.noreply.github.com>
2024-03-22 02:56:57 +0000

    day8 카이사르

--- main.py
+++ main.py
@@ -13,17 +13,17 @@ def encrypt (plain_text, shift_amount):
     new_letter = alphabet[new_position]
     cipher_text += new_letter
   print(f"The encoded text is {cipher_text}")
-    #TODO-2: Inside the 'encrypt' function, shift each letter of the 'text' forwards in the alphabet by the shift amount and print the encrypted text.  
-    #e.g. 
-    #plain_text = "hello"
-    #shift = 5
-    #cipher_text = "mjqqt"
-    #print output: "The encoded text is mjqqt"
+   
+def decrypt(cipher_text, shift_amount):
+  plain_text = ""
+  for letter in cipher_text:
+    position = alphabet.index(letter)
+    new_position = position - shift_amount
+    plain_text += alphabet[new_position]
+  print(f"The decoded text is {plain_text}")
 
-    ##HINT: How do you get the index of an item in a list:
-    #https://stackoverflow.com/questions/176918/finding-the-index-of-an-item-in-a-list
 
-    ##🐛Bug alert: What happens if you try to encode the word 'civilization'?🐛
-
-#TODO-3: Call the encrypt function and pass in the user inputs. You should be able to test the code and encrypt a message. 
-encrypt (plain_text=text , shift_amount=shift)
+if direction == "encode" :
+  encrypt(plain_text=text, shift_amount=shift)
+else :
+  decrypt(cipher_text=text, shift_amount=shift)
