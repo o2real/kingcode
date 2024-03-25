4ec09db73d2b694ce738135fab50bdae950b3efd
o2real <136673119+o2real@users.noreply.github.com>
2024-03-25 14:50:42 +0000

    카이사르 좀 하자

--- main.py
+++ main.py
@@ -14,16 +14,16 @@ def encrypt (plain_text, shift_amount):
     cipher_text += new_letter
   print(f"The encoded text is {cipher_text}")
    
-def decrypt(cipher_text, shift_amount):
-  plain_text = ""
-  for letter in cipher_text:
-    position = alphabet.index(letter)
-    new_position = position - shift_amount
-    plain_text += alphabet[new_position]
-  print(f"The decoded text is {plain_text}")
+# def decrypt(cipher_text, shift_amount):
+#   plain_text = ""
+#   for letter in cipher_text:
+#     position = alphabet.index(letter)
+#     new_position = position - shift_amount
+#     plain_text += alphabet[new_position]
+#   print(f"The decoded text is {plain_text}")
 
 
-if direction == "encode" :
-  encrypt(plain_text=text, shift_amount=shift)
-else :
-  decrypt(cipher_text=text, shift_amount=shift)
+# if direction == "encode" :
+#   encrypt(plain_text=text, shift_amount=shift)
+# else :
+#   decrypt(cipher_text=text, shift_amount=shift)
