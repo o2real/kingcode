079209beb8acd47be4e4265ecdc325e51f6d5c74
o2real <136673119+o2real@users.noreply.github.com>
2024-04-16 08:19:30 +0000

    day9(2)

--- main.py
+++ main.py
@@ -1,1 +1,27 @@
-programming_dictionary = {"Bug": "An error in a program that prevents the program from running as expected.", "Function": "A piece of code that you can easily call over and over again."}
+programming_dictionary = {
+  "Bug": "An error in a program that prevents the program from running as expected.", 
+  "Function": "A piece of code that you can easily call over and over again.",
+}
+
+print(programming_dictionary["Bug"])
+
+programming_dictionary["Loop"] = "The action of doing something over and over again."
+
+print(programming_dictionary)
+
+
+#Create an empty dictionary.
+empty_dictionary = {}
+
+#Wipe
+# programming_dictionary = {}
+# print(programming_dictionary)
+
+#Edit
+programming_dictionary["Bug"] = "A moth in your computer."
+print(programming_dictionary)
+
+#loop
+for key in programming_dictionary:
+  print(key)
+  print(programming_dictionary[key])
