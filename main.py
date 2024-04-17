5b459b54591b40149eb570cf04f7692470d01c1f
o2real <136673119+o2real@users.noreply.github.com>
2024-04-17 14:35:55 +0000

    day9(3)

--- main.py
+++ main.py
@@ -25,3 +25,54 @@ print(programming_dictionary)
 for key in programming_dictionary:
   print(key)
   print(programming_dictionary[key])
+
+#--------------------------------------------- 
+
+student_scores = {
+  "Harry": 81,
+  "Ron": 78,
+  "Hermione": 99, 
+  "Draco": 74,
+  "Neville": 62,
+}
+
+student_grades = {}
+
+
+
+for student in student_scores :
+  score = student_scores[student]
+  if score > 90 :
+    student_grades[student] = "Outstanding"
+  elif score > 80 :
+    student_grades[student] = "Exceeds Expectations"
+  elif score > 70 :
+    student_grades[student] = "Acceptable"
+  else :
+    student_grades[student] = "Fail"
+
+
+print(student_grades)
+
+
+#--------------------
+
+travel_log = {
+  "France" :{"cities_visited" : ["Paris", "Lille", "Dijon"], "total_visits" :12},
+  "Germany" :{"cities_visited" :["Berlin", "Hamburg", "Stuttgart"], "total_visits" :12},
+}
+
+#Nesting Dictionary in a list
+
+travel_log = [
+  {
+    "country" : "France", 
+    "cities_visited" : ["Paris", "Lille", "Dijon"], 
+    "total_visits" : 12
+  },
+  {
+    "country" : "Germany", 
+    "cities_visited" : ["Berlin", "Hamburg", "Stuttgart"],
+    "total_visits" : 12
+  },
+]
