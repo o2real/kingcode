653ba6465e99f2b25af00023d8f9c8f3782fd26d
o2real <136673119+o2real@users.noreply.github.com>
2024-04-30 10:09:15 +0000

    day10 (2)

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 def format_name(f_name, l_name):
+  """Take a first and last name and format it to return the title case version of the name."""
   if f_name == "" or l_name == "" :
     return "You didn't provide vaild inputs."
 
@@ -8,3 +9,36 @@ def format_name(f_name, l_name):
 
 
 print(format_name(input("What is your first name?"), input("What is your last name?")))
+
+format_name()  #""" """ 는 메모같은거임 독스트링
+
+#-----------------------
+
+
+def is_leap(year):
+  if year % 4 == 0:
+    if year % 100 == 0:
+      if year % 400 == 0:
+        return True
+      else:
+        return False
+    else:
+      return True
+  else:
+    return False
+
+# TODO: Add more code here 👇
+def days_in_month(year, month):
+  month_days = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31] 
+  if month == 2 and is_leap(year) :
+    return 29
+  else:
+    return month_days[month - 1]
+#🚨 Do NOT change any of the code below 
+year = int(input()) # Enter a year
+month = int(input()) # Enter a month
+days = days_in_month(year, month)
+print(days)
+
+
+#-------------------------
